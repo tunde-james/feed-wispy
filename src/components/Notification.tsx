149c3201168ba6524767f060e98c5f3aef384ac6
Tunde James <tundejames04@gmail.com>
2023-10-03 09:15:31 +0100

    Implement notification component design

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,5 +1,9 @@
 function Notification() {
-  return <div>Notification</div>;
+  return (
+    <div className="flex h-12 cursor-pointer items-center justify-center bg-red-500 px-4 text-center text-sm text-white md:text-base">
+      Free delivery for all orders above $50. Order your food now!
+    </div>
+  );
 }
 
 export default Notification;
