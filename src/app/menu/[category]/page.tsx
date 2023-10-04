79c66ac915e00bbfd1b75c68b982b04189497a40
Tunde James <tundejames04@gmail.com>
2023-10-04 20:00:33 +0100

    Create the menu page using a dummy data

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { pizzas } from "@/data";
+import Image from "next/image";
+import Link from "next/link";
 
 function CategoryPage() {
-  return <div>CategoryPage</div>;
+  return (
+    <main className="flex flex-wrap text-red-500">
+      {pizzas.map((item) => (
+        <Link
+          href={`/product/${item.id}`}
+          key={item.id}
+          className="group flex h-[60vh] w-full flex-col justify-between border-b-2 border-r-2 border-red-500 p-4 even:bg-fuchsia-50 sm:w-1/2 lg:w-1/3"
+        >
+          {/* Image Container */}
+          <div className="relative h-[80%]">
+            {item.img && (
+              <Image src={item.img} alt="" fill className="object-contain" />
+            )}
+          </div>
+
+          {/* Text Container */}
+          <div className="flex items-center justify-between">
+            <h1 className="p-2 text-xl font-bold uppercase">{item.title}</h1>
+            <h2 className="text-xl group-hover:hidden">${item.price}</h2>
+            <button className="hidden rounded-md bg-red-500 px-2 py-1 uppercase text-white group-hover:block">
+              Add to Cart
+            </button>
+          </div>
+        </Link>
+      ))}
+    </main>
+  );
 }
 
 export default CategoryPage;
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Link from "next/link";
+import { menu } from "@/data";
 
 function MenuPage() {
   return (
-    <div>MenuPage</div>
-  )
+    <main className="flex h-[calc(100vh-6rem)] flex-col items-center p-4 md:h-[calc(100vh-9rem)] md:flex-row lg:px-20 xl:px-40">
+      {menu.map((category) => (
+        <Link
+          href={`/menu/${category.slug}`}
+          key={category.id}
+          className="h-1/3 w-full bg-cover p-8 md:h-1/2"
+          style={{ backgroundImage: `url(${category.img})` }}
+        >
+          <div className={`text-${category.color} w-1/2`}>
+            <h2 className="text-sm font-bold uppercase">{category.title}</h2>
+            <p className="my-1 text-sm">{category.desc}</p>
+            <button
+              className={`hidden xl:block bg-${category.color} rounded-md px-4 py-2 text-${category.color === "black" ? "white" : "red-500"}`}
+            >
+              Explore
+            </button>
+          </div>
+        </Link>
+      ))}
+    </main>
+  );
 }
 
-export default MenuPage
+export default MenuPage;
