273a68ca107f17cb65bb9bb5fc586a2c58503057
Tunde James <tundejames04@gmail.com>
2023-10-09 16:29:54 +0100

    Fetch the categories from postgres DB

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,9 +1,24 @@
+import { MenuType } from "@/types/types";
 import Link from "next/link";
-import { menu } from "@/data";
 
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
 
+  if (!res.ok) {
+    const message = `An error has occured: ${res.status}`;
+    throw new Error(message);
+  }
+
+  const json = await res.json();
+
+  return json;
+}
 
 async function MenuPage() {
+  const menu: MenuType = await getData();
+
   return (
     <main className="flex h-[calc(100vh-6rem)] flex-col items-center p-4 md:h-[calc(100vh-9rem)] md:flex-row lg:px-20 xl:px-40">
       {menu.map((category) => (
@@ -14,10 +29,17 @@ async function MenuPage() {
           style={{ backgroundImage: `url(${category.img})` }}
         >
           <div className={`text-${category.color} w-1/2`}>
-            <h2 className="text-sm font-bold uppercase">{category.title}</h2>
-            <p className="my-1 text-sm">{category.desc}</p>
+            <h2 className="text-sm font-bold uppercase lg:text-lg">
+              {category.title}
+            </h2>
+            <p className="lg:text- my-1 text-sm">{category.desc}</p>
             <button
-              className={`hidden xl:block bg-${category.color} rounded-md px-4 py-2 text-${category.color === "black" ? "white" : "red-500"}`}
+              className={`hidden rounded-md px-4
+               py-2 lg:block ${
+                 category.color === "white"
+                   ? "bg-white text-red-500"
+                   : "bg-black text-white"
+               }`}
             >
               Explore
             </button>
